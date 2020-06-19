872f15ea1e01dfa5004f0f05dd89029e80d3b8e9
Sravya2007 <sravyamarepally@outlook.com>
2020-06-19 17:49:16 +0530

    Bird trajectory added

--- sketch.js
+++ sketch.js
@@ -8,6 +8,8 @@ var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
 
+var gameState = "onSling";
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -39,14 +41,12 @@ function setup(){
 
     bird = new Bird(200,50);
 
-    //log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
@@ -64,21 +64,17 @@ function draw(){
 
     bird.display();
     platform.display();
-    //log6.display();
     slingshot.display();    
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gameState !== "launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
 
 
 function mouseReleased(){
     slingshot.fly();
-}
-
-function keyPressed(){
-    if(keyCode === 32){
-        slingshot.attach(bird.body);
-    }
+    gameState = "launched";
 }
